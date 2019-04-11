d1054b31b5ab1195e2b83ed0a16b2633e9c441a6
lobster2012-user <lobster2012.user@gmail.com>
2019-04-11 23:14:03 +0300

    add ext-s

---
+++ Lobster.Home.DependencyInjection.ServiceTypeProvider/PluginServiceTypeProviderExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lobster.Home.DependencyInjection
+{
+    public static class PluginServiceTypeProviderExtensions
+    {
+        
+    }
+}
